f9fa375e76307598c8d75f47c4863bd65ad0c620
Nikunj Gupta <nikunj.gupta@hashedin.com>
2020-12-11 14:56:03 +0530

    append and write endpoints created

---
+++ src/cli.js
@@ -0,0 +1,89 @@
+import arg from "arg";
+import restify from "restify";
+import errs from "restify-errors";
+import path from "path";
+import fs from "fs";
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--port": String,
+      "--folder": String,
+      "-p": "--port",
+      "-f": "--folder",
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    port: args["--port"] || "8000",
+    folder: args["--folder"] || process.cwd(),
+  };
+}
+
+export function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+
+  const server = restify.createServer();
+  server.use(restify.plugins.queryParser());
+  server.use(
+    restify.plugins.bodyParser({
+      mapParams: true,
+    })
+  );
+  server.use(restify.plugins.acceptParser(server.acceptable));
+
+  const append = (req, res, next) => {
+    if (req.query && req.query.file) {
+      const file = path.join(options.folder, req.query.file);
+      if (req.body && req.body.length > 0) {
+        const data = req.body;
+        fs.appendFile(file, data, function (err) {
+          if (err) {
+            next(
+              new errs.InternalServerError("Unable to complete the request")
+            );
+          }
+          res.send("Success");
+        });
+      } else {
+        next(new errs.BadRequestError("Empty request body"));
+      }
+    } else {
+      next(new errs.BadRequestError("file query param missing"));
+    }
+  };
+
+  const write = (req, res, next) => {
+    if (req.query && req.query.file) {
+      const file = path.join(options.folder, req.query.file);
+      if (req.body && req.body.length > 0) {
+        const data = req.body;
+        fs.writeFile(file, data, function (err) {
+          if (err) {
+            next(
+              new errs.InternalServerError("Unable to complete the request")
+            );
+          }
+          res.send("Success");
+        });
+      } else {
+        next(new errs.BadRequestError("Empty request body"));
+      }
+    } else {
+      next(new errs.BadRequestError("file query param missing"));
+    }
+  };
+
+  server.post("/append", append);
+  server.post("/write", write);
+
+  server.listen(options.port, function () {
+    console.log("rest-write-to-fs listening at ");
+    console.log(
+      "\x1b[32m",
+      `\thttp://localhost:${options.port} \n\thttp://127.0.0.1:${options.port}`
+    );
+  });
+}
