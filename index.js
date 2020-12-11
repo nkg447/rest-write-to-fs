c52062ddbca7c7f1b02077639a0e51a20980d035
Nikunj Gupta <nikunj.gupta@hashedin.com>
2020-12-11 19:44:57 +0530

    bug fix

--- index.js
+++ index.js
@@ -61,8 +61,9 @@ function cli(args) {
   const write = (req, res, next) => {
     if (req.query && req.query.file) {
       const file = path.join(options.folder, req.query.file);
-      if (req.body && req.body.length > 0) {
-        const data = req.body;
+      if (req.body && req.headers["content-length"] > 0) {
+        const data =
+          typeof req.body === "object" ? JSON.stringify(req.body) : req.body;
         fs.writeFile(file, data, function (err) {
           if (err) {
             next(
